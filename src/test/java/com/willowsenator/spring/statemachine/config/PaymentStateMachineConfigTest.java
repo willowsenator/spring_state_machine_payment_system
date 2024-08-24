195e4f80a5a8a6a294c4908fe237161207af46e5
willowsenator <willowsenator@gmail.com>
2024-08-25 01:34:20 +0200

    Config transations and states

---
+++ src/test/java/com/willowsenator/spring/statemachine/config/PaymentStateMachineConfigTest.java
@@ -0,0 +1,51 @@
+package com.willowsenator.spring.statemachine.config;
+
+import com.willowsenator.spring.statemachine.domain.PaymentEvent;
+import com.willowsenator.spring.statemachine.domain.PaymentState;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.statemachine.config.StateMachineFactory;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+
+@SpringBootTest
+class PaymentStateMachineConfigTest {
+    @Autowired
+    StateMachineFactory<PaymentState, PaymentEvent> factory;
+
+    @Test
+    void testNewStateMachine() {
+        var sm = factory.getStateMachine(UUID.randomUUID());
+        sm.startReactively().block();
+
+        System.out.println(sm.getState().toString());
+
+        var preAuthEventMono = Mono.just(MessageBuilder.withPayload(PaymentEvent.PRE_AUTHORIZE).build());
+        var resultPreAuthEventFlux = sm.sendEvent(preAuthEventMono);
+
+        resultPreAuthEventFlux.subscribe(result -> {
+           var state = result.getRegion().getState();
+           System.out.println(state.toString());
+       });
+
+       var preAuthApprovedEventMono = Mono.just(MessageBuilder.withPayload(PaymentEvent.PRE_AUTH_APPROVED).build());
+       var resultPreAuthApprovedEventFlux = sm.sendEvent(preAuthApprovedEventMono);
+
+       resultPreAuthApprovedEventFlux.subscribe(result -> {
+           var state = result.getRegion().getState();
+           System.out.println(state.toString());
+       });
+
+       var preAuthDeclinedEventMono = Mono.just(MessageBuilder.withPayload(PaymentEvent.PRE_AUTH_DECLINED).build());
+       var resultPreAuthDeclinedEventFlux = sm.sendEvent(preAuthDeclinedEventMono);
+
+         resultPreAuthDeclinedEventFlux.subscribe(result -> {
+              var state = result.getRegion().getState();
+              System.out.println(state.toString());
+         });
+    }
+}
